99eb4e2a19ec9c3c567facfacf78f8a8370c878f
Nikos <nvourdoumpas@hotmail.com>
2024-07-13 20:11:38 +0300

    feat: add mdi lib wich use vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,10 @@
 import { createApp } from "vue";
 
 // Vuetify
-import "vuetify/styles";
-import { createVuetify } from "vuetify";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
+import vuetify from "@/plugins/vuetify";
 
 // Components
 import App from "@/App.vue";
 import router from "@/router/router.ts";
-
-const vuetify = createVuetify({
-  components,
-  directives,
-});
 
 createApp(App).use(vuetify).use(router).mount("#app");
