96b186c28a373c6643965b5ceeb6ca9f168bd15f
Nikos <nvourdoumpas@hotmail.com>
2024-08-04 14:30:38 +0300

    feat: complete implementation of pokemon page

--- src/types/getPokemonResponse.ts
+++ src/types/getPokemonResponse.ts
@@ -4,9 +4,10 @@ export default interface getPokemonsResponse {
   base_experience: number;
   height: number;
   weight: number;
-  abilities: NamedAPIResource[];
-  moves: NamedAPIResource[];
-  types: Types[];
+  abilities: Ability[];
+  moves: Move[];
+  types: Type[];
+  stats: Stat[];
 }
 
 type NamedAPIResource = {
@@ -14,7 +15,30 @@ type NamedAPIResource = {
   url: string;
 };
 
-type Types = {
+type Ability = {
+  is_hidden: boolean;
+  slot: number;
+  ability: NamedAPIResource;
+};
+
+type Move = {
+  move: NamedAPIResource;
+  version_group_details: MoveVersion;
+};
+
+type MoveVersion = {
+  move_learn_method: NamedAPIResource;
+  version_group: NamedAPIResource;
+  level_learned_at: number;
+};
+
+type Type = {
   slot: number;
   type: NamedAPIResource;
+};
+
+type Stat = {
+  stat: NamedAPIResource;
+  effort: number;
+  base_stat: number;
 };
--- src/views/Pokemon.vue
+++ src/views/Pokemon.vue
@@ -9,7 +9,7 @@
           <v-col cols="12" md="4">
             <v-img
               color="surface-variant"
-              height="400"
+              height="100%"
               :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`"
             >
             </v-img>
@@ -17,39 +17,109 @@
           <v-col cols="12" md="8">
             <v-card flat height="100%" color="transparent">
               <v-card-title
-                class="mt-4 text-h4 text-uppercase font-weight-black"
+                class="mt-4 text-h5 text-uppercase font-weight-black"
               >
+                <span class="font-italic"># {{ pokemon.id }}</span> -
                 {{ pokemon.name }}
               </v-card-title>
-              <v-card-subtitle class="text-h6 font-italic">
-                # {{ pokemon.id }}
-              </v-card-subtitle>
+              <v-divider class="mr-6"></v-divider>
               <v-card-text>
                 <v-row>
-                  <v-col cols="12">
-                    <v-row>
-                      <v-col cols="2" class="mt-4"> Types: </v-col>
-                      <v-col
-                        v-for="pokemonType in pokemon.types"
-                        :key="pokemonType.slot"
-                        cols="1"
-                      >
-                        <v-sheet
-                          :elevation="4"
-                          :height="50"
-                          :width="40"
-                          rounded
-                          color="black"
-                        >
-                          <v-img
-                            :alt="pokemonType.type.name"
-                            :src="getTypeImage(pokemonType.type.name)"
-                            cover
-                          ></v-img>
-                        </v-sheet>
-                      </v-col>
-                    </v-row>
+                  <h4 class="mt-8 mr-4">Types:</h4>
+                  <v-col
+                    v-for="pokemonType in pokemon.types"
+                    :key="pokemonType.slot"
+                    cols="1"
+                    class="mx-5"
+                  >
+                    <v-sheet
+                      :elevation="4"
+                      :height="52"
+                      :width="45"
+                      class="pa-1"
+                      color="black"
+                      rounded
+                    >
+                      <v-img
+                        :alt="pokemonType.type.name"
+                        :src="getTypeImage(pokemonType.type.name)"
+                        cover
+                      ></v-img>
+                    </v-sheet>
                   </v-col>
+                </v-row>
+                <v-row class="mt-6">
+                  <h4 class="mr-3">Dimensions:</h4>
+                  <v-icon
+                    icon="mdi-arrow-expand-vertical"
+                    class="mr-4"
+                  ></v-icon>
+                  {{ pokemon.height }}
+                  <v-icon icon="mdi-weight" class="mx-4"></v-icon>
+                  {{ pokemon.weight }}
+                </v-row>
+                <v-row class="mt-9">
+                  <h4 class="mr-9">Base XP:</h4>
+                  {{ pokemon.base_experience }}
+                </v-row>
+                <v-row class="mt-9">
+                  <h4 class="mr-6 mt-1">Abilities:</h4>
+                  <v-chip
+                    variant="elevated"
+                    v-for="(ability, index) in pokemon.abilities"
+                    :key="index"
+                    class="mx-2"
+                    color="white"
+                  >
+                    {{ ability.ability.name }}
+                  </v-chip>
+                </v-row>
+                <v-row class="mt-9">
+                  <h4 class="mb-2">Stats:</h4>
+                  <v-table class="text-caption" density="compact" dense>
+                    <tbody>
+                      <tr>
+                        <td></td>
+                        <td v-for="(stat, index) in pokemon.stats" :key="index">
+                          {{ stat.stat.name }}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>base</td>
+                        <td
+                          v-for="(stat, index) in pokemon.stats"
+                          :key="index"
+                          align="center"
+                        >
+                          {{ stat.base_stat }}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>effort</td>
+                        <td
+                          v-for="(stat, index) in pokemon.stats"
+                          :key="index"
+                          align="center"
+                        >
+                          {{ stat.effort }}
+                        </td>
+                      </tr>
+                    </tbody>
+                  </v-table>
+                </v-row>
+                <v-row class="mt-9">
+                  <h4 class="mr-6 mt-1">Moves:</h4>
+                  <v-chip
+                    variant="tonal"
+                    label
+                    v-for="(move, index) in pokemon.moves"
+                    :key="index"
+                    class="ma-1"
+                    color="white"
+                    size="x-small"
+                  >
+                    {{ move.move.name }}
+                  </v-chip>
                 </v-row>
               </v-card-text>
             </v-card>
